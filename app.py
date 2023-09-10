8f3f4a7fe7af3bc1bda58a63283dca3fef034866
Shayyan Bilal <shayyanbilal17@gmail.com>
2023-09-10 20:54:37 +0500

    abc

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app= Flask(__name__)
+
+@app.route("/")
+def home():
+	return "This is a Home page"
+
+@app.route("/page1")
+def Page2():
+	message="Page 1"
+	return render_template("index.html",message=message)
+	
+if __name__=="__main__":
+	app.run(debug=True, port=5000)
