a759f3fee19fec621e8a4242554163ef61a7cc6b
Shayyan Bilal <shayyanbilal17@gmail.com>
2023-09-10 21:34:08 +0500

    secondcommit

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app= Flask(__name__)
 def home():
 	return "This is a Home page"
 
+@app.route("/senso")
+def home2():
+	return "This is a senso page"
+
+
 @app.route("/page1")
 def Page2():
 	message="Page 1"
